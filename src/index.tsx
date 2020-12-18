bc863075d7d5ed852caa646092886437a929412a
Nik Schaefer <nikkschaefer@gmail.com>
2020-12-17 20:42:31 -0600

    Inital

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
+import { useState } from 'react';
 
+let content: number[] = [];
 
+function Render(props: { arr: number[] }): any {
+  let out: any[] = [];
+  for (let i: number = 0; i < props.arr.length; i++) {
+    out.push(
+      <div className='inside' style={{ height: props.arr[i] * 8 }}></div>
+    )
+  }
+  return out
+}
 function App() {
+
+
+  const [con, setContent] = useState<any>([])
+
+  window.onload = function () {
+    for (let j = 0; j < 50; j++) {
+      content.push(j)
+      setContent([...con, 8])
+    }
+  }
+
   return (
-    <div>
+    <div className='home'>
+      <div className='content-div'>
+        <Render arr={content} />
+      </div>
 
     </div>
   );
 }
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
